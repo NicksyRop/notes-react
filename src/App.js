3c696695a557bbf6ba4cd4bf0bcfe68ec851da04
NicksyRop <nicksonkipkorir25@gmail.com>
2022-01-28 09:56:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import Note from './components/Note'
+import React, { useState, useEffect } from "react";
+import Note from "./components/Note";
+import axios from "axios";
 
-const App = ({ notes }) => {
+const App = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/notes")
+      .then((res) => setNotes(res))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
-            <Note key={note.id} note={note} />
-        )}
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
